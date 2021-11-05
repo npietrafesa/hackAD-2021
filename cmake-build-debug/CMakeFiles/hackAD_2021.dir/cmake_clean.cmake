692b49e99d1d558f1b6fad67b17981c3e72835c7
Noah Pietrafesa <npietrafesa@Noahs-MacBook-Pro.local>
2021-11-05 18:02:23 +0400

    testing testing 123

---
+++ cmake-build-debug/CMakeFiles/hackAD_2021.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hackAD_2021.dir/main.cpp.o"
+  "hackAD_2021"
+  "hackAD_2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hackAD_2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hackAD_2021.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hackAD_2021.
+# This may be replaced when dependencies are built.
