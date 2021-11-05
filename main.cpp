7c334e1ad42ae76f4dfbd89bb3dfee9a51ce2bcb
npietrafesa <npietrafesa110@gmail.com>
2021-11-05 18:55:11 +0400

    fleshing out user

--- main.cpp
+++ main.cpp
@@ -9,10 +9,39 @@ private:
     string firstName;
     string lastName;
     int mealSwipes;
-
+    bool isGiving;
+    bool isReceiving;
+    bool isActive;
+public:
+    User() {
+        cout << "Enter First Name: " << endl;
+        cin >> firstName;
+        cout << "Enter Last Name: " << endl;
+        cin >> lastName;
+        cout << "Enter netid: " << endl;
+        cin >> netid;
+        cout << "Enter password: " << endl;
+        cin >> password;
+        cout << "How many swipes do you have?" << endl;
+        cin >> mealSwipes;
+    }
+    string getFirstName() {
+        return firstName;
+    }
+    string getLastName() {
+        return lastName;
+    }
+    int getSwipes() {
+        return mealSwipes;
+    }
+    void printInfo() {
+        cout << netid << " " << password << " " << firstName << " " << lastName << " " << mealSwipes << endl;
+    }
 };
 
 int main() {
     cout << "Hello, World!" << endl;
+//    User usr;
+//    usr.printInfo();
     return 0;
 }
