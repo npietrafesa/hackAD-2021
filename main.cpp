fdbf6ecff3ad52fc0591bf8c87cad7216e84a5ab
npietrafesa <npietrafesa110@gmail.com>
2021-11-05 18:15:14 +0400

    added barebones user

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+class User {
+private:
+    string netid;
+    string password;
+    string firstName;
+    string lastName;
+    int mealSwipes;
+
+};
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
